a4d834661f4580fdce62c8805b7987f79144e00c
jack <2913895+tiga1231@users.noreply.github.com>
2023-11-09 01:34:21 -0600

    update

--- run_describe_neurons.sh
+++ run_describe_neurons.sh
@@ -1,7 +1,10 @@
 python describe_neurons.py \
     --d_probe='imagenet_val' \
     --target_layers='conv1,layer1,layer2,layer3,layer4' \
-    --concept_set='data/20k_and_imagenet.txt'
+    --concept_set='data/wordnet_hierarchy.txt'
+
+
+    # --concept_set='data/20k_and_imagenet.txt'
     # --concept_set='data/20k.txt'
     # --concept_set='data/nouns_and_adjectives.txt'
     # --pool_mode='None'
